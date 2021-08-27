f0a38c4be2af9eeb27af1e3fce087af20844a7d2
GaoLeon <648653259@qq.com>
2021-08-27 14:03:49 +0800

    sssm 生成

--- Assets/HomeWork/HW5/ScreenSpaceShadowmap.cs
+++ Assets/HomeWork/HW5/ScreenSpaceShadowmap.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class ScreenSpaceShadowmap : BaseShadowCamera
 {
+    public RenderTexture mainCameraDepthTexture;
+
+    public Material SSSMGenerator;
+
     protected FrustumCorners mainCamera_fcs, shadowCamera_fcs;
 
-    protected RenderTexture shadowTexture;
+    protected RenderTexture lightSpaceDepthTexture;
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +18,9 @@ public class ScreenSpaceShadowmap : BaseShadowCamera
         InitShadowCam();
         InitFrustumCorners(ref mainCamera_fcs);
         InitFrustumCorners(ref shadowCamera_fcs);
-        InitShaderTexture(ref shadowTexture, 0);
+        InitDepthTexture(ref lightSpaceDepthTexture, 0);
+        mainCameraDepthTexture = new RenderTexture(Screen.width, Screen.height, 32);
+        Camera.main.depthTextureMode |= DepthTextureMode.Depth;
         Shader.SetGlobalFloat("_CascadedLevels", 1);
     }
 
@@ -24,13 +30,15 @@ public class ScreenSpaceShadowmap : BaseShadowCamera
         CalcMainCameraFrustumCorners(Camera.main.nearClipPlane, Camera.main.farClipPlane, ref mainCamera_fcs);
         CalcShadowCameraFrustum(ref shadowCamera_fcs, ref mainCamera_fcs);
         ResetShadowCamera(ref shadowCamera_fcs);
-        ShadowTextureRender(shadowTexture, 0);
+        ShadowTextureRender(lightSpaceDepthTexture, 0);
+        GenerateSSSM();
     }
 
-    void OnDestroy()
+    void OnDisable()
     {
         shadowMapCam = null;
-        DestroyImmediate(shadowTexture);
+        DestroyImmediate(lightSpaceDepthTexture);
+        DestroyImmediate(mainCameraDepthTexture);
     }
 
     void OnDrawGizmos()
@@ -40,5 +48,45 @@ public class ScreenSpaceShadowmap : BaseShadowCamera
 
         Gizmos.color = Color.cyan;
         DrawFrustum(shadowCamera_fcs.nearCorners, shadowCamera_fcs.farCorners);
+    }
+
+    protected void GenerateSSSM()
+    {
+        Matrix4x4 frustumCorners = Matrix4x4.identity;
+
+        float fov = Camera.main.fieldOfView;
+        float near = Camera.main.nearClipPlane;
+        float aspect = Camera.main.aspect;
+
+        float halfHeight = near * Mathf.Tan(fov * 0.5f * Mathf.Deg2Rad);
+        Vector3 toRight = Camera.main.transform.right * halfHeight * aspect;
+        Vector3 toTop = Camera.main.transform.up * halfHeight;
+
+        Vector3 topLeft = Camera.main.transform.forward * near + toTop - toRight;
+        float scale = topLeft.magnitude / near;
+
+        topLeft.Normalize();
+        topLeft *= scale;
+
+        Vector3 topRight = Camera.main.transform.forward * near + toRight + toTop;
+        topRight.Normalize();
+        topRight *= scale;
+
+        Vector3 bottomLeft = Camera.main.transform.forward * near - toTop - toRight;
+        bottomLeft.Normalize();
+        bottomLeft *= scale;
+
+        Vector3 bottomRight = Camera.main.transform.forward * near + toRight - toTop;
+        bottomRight.Normalize();
+        bottomRight *= scale;
+
+        frustumCorners.SetRow(0, bottomLeft);
+        frustumCorners.SetRow(1, bottomRight);
+        frustumCorners.SetRow(2, topRight);
+        frustumCorners.SetRow(3, topLeft);
+
+        SSSMGenerator.SetMatrix("_FrustumCornersRay", frustumCorners);
+
+        Graphics.Blit(mainCameraDepthTexture, mainCameraDepthTexture, SSSMGenerator);
     }
 }
---
+++ Assets/Shaders/SSSMGenerator.shader
@@ -0,0 +1,83 @@
+﻿Shader "Unlit/SSSMGenerator"
+{
+    SubShader
+    {
+        Pass
+        {
+            ZTest Always Cull Off ZWrite Off
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 texcoord : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
+                half2 uv_depth : TEXCOORD1;
+                float4 interpolatedRay : TEXCOORD2;
+            };
+
+            sampler2D _CustomShadowMap0;
+            float4 _CustomShadowMap0_TexelSize;
+            float4x4 _CustomLightSpaceMatrix0;
+
+            sampler2D _CameraDepthTexture;
+            float4x4 _FrustumCornersRay;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.texcoord;
+
+                o.uv_depth = v.texcoord;
+
+                int index = 0;
+                if (v.texcoord.x < 0.5 && v.texcoord.y < 0.5) {
+                    index = 0;
+                } else if (v.texcoord.x > 0.5 && v.texcoord.y < 0.5) {
+                    index = 1;
+                } else if (v.texcoord.x > 0.5 && v.texcoord.y > 0.5) {
+                    index = 2;
+                } else {
+                    index = 3;
+                }
+                
+                o.interpolatedRay = _FrustumCornersRay[index];
+
+                return o;
+            }
+
+            float getDepth(float4 lightClipPos)
+            {
+                float2 uv = 0.5 * lightClipPos.xy/lightClipPos.w + 0.5;
+                float depth = saturate(lightClipPos.z/lightClipPos.w);
+                #if defined (SHADER_TARGET_GLSL) 
+                    depth = depth * 0.5 + 0.5;
+                #elif defined (UNITY_REVERSED_Z)
+                    depth = 1 - depth;
+                #endif
+                float samplerValue = tex2D(_CustomShadowMap0, uv).r;
+                return samplerValue < depth ? 0 : 1;
+                // return samplerValue;
+            }
+
+            float4 frag (v2f i) : SV_Target
+            {
+                float linearDepth = LinearEyeDepth(SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv_depth));
+                float3 worldPos = _WorldSpaceCameraPos + linearDepth * i.interpolatedRay.xyz;
+                float4 lightPos = mul(_CustomLightSpaceMatrix0, float4(worldPos, 1));
+                return float4(getDepth(lightPos).xxx, 1);
+            }
+            ENDCG
+        }
+    }
+}

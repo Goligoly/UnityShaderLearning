358a485b45c6a68c0313b9060e1280954d473a3b
GaoLeon <648653259@qq.com>
2022-01-13 18:10:54 +0800

    依赖删除

---
+++ publishPackage.bat
@@ -0,0 +1,9 @@
+Set ToolName=upm
+Set ToolVersion=0.0.3
+Set ToolAssetPath=Assets/testpackage
+
+git subtree split --prefix=%ToolAssetPath% --branch %ToolName%
+git tag %ToolVersion% %ToolName%
+
+git push origin %ToolName% %ToolVersion%
+git push origin %ToolName%
